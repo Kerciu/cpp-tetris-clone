7a955e63959f23952bde56fad344ad4867b195b3
Kerciu <Kerciuuu@gmail.com>
2024-08-03 12:16:18 +0200

    added gradient to blocks, but when they are not falling gradient disappears

--- src/utils/colors.cpp
+++ src/utils/colors.cpp
@@ -1,13 +1,13 @@
 #include "colors.h"
 
-const sf::Color dark_grey = {26, 31, 40};
-const sf::Color green = sf::Color::Green;
-const sf::Color red = sf::Color::Red;
-const sf::Color orange = {226, 116, 17};
-const sf::Color yellow = sf::Color::Yellow;
-const sf::Color magenta = sf::Color::Magenta;
-const sf::Color cyan = sf::Color::Cyan;
-const sf::Color blue = sf::Color::Blue;
+const sf::Color dark_grey = sf::Color(0x373A40ff);
+const sf::Color green = sf::Color(0xDEF9C4ff);
+const sf::Color red = sf::Color(0xF5004Fff);
+const sf::Color orange = sf::Color(0xFFB200ff);
+const sf::Color yellow = sf::Color(0xF6FB7Aff);
+const sf::Color magenta = sf::Color(0xB692C2ff);
+const sf::Color cyan = sf::Color(0x59D5E0ff);
+const sf::Color blue = sf::Color(0x3ABEF9ff);
 const int OFFSET = 1;
 
 
@@ -18,5 +18,22 @@ std::vector<sf::Color> get_cell_colors()
 
 sf::Color get_appropriate_gradient_color(sf::Color top_color)
 {
-    return sf::Color();
+    if (top_color == dark_grey)
+        return sf::Color(0x686D76ff);
+    else if (top_color == green)
+        return sf::Color(0x50B498ff);
+    else if (top_color == red)
+        return sf::Color(0xEF5A6Fff);
+    else if (top_color == orange)
+        return sf::Color(0xEB5B00ff);
+    else if (top_color == yellow)
+        return sf::Color(0xFFDE4Dff);
+    else if (top_color == magenta)
+        return sf::Color(0xFF4E88ff);
+    else if (top_color == cyan)
+        return sf::Color(0x8DDFCBff);
+    else if (top_color == blue)
+        return sf::Color(0x050C9Cff);
+    else
+        return sf::Color::Transparent;
 }
